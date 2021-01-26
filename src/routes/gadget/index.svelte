8fc983094291605bc4edc5474be2c343a6de2ea8
zhangmingrui <zhangmingrui@mokahr.com>
2021-01-26 20:52:53 +0800

    添加了marked

---
+++ src/routes/gadget/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+</script>
+
+<script>
+	// export let posts;
+</script>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
+
+<svelte:head>
+	<title>Gadget</title>
+</svelte:head>
+
+<h1>Recent posts</h1>
+
+<ul>
+	<li><a rel="prefetch" href="gadget/marked">marked</a></li>
+</ul>
---
+++ src/routes/gadget/marked.svelte
@@ -0,0 +1,20 @@
+<script>
+import marked from 'marked'
+marked.setOptions({
+    renderer:new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+})
+
+// document.getElementById('content').innerHtml = marked('# Marked in browser\n\nRendered by **marked**');
+
+</script>
+
+<h1>About this site</h1>
+
+<p>This is the 'marked' page. There's not much here.</p>
+<div id = "content"></div>
