3412de8383667e7a5d0e56f88007da8e1e197fdf
DESKTOP-QB7234L\Rae <zhangmingrui@mokahr.com>
2021-05-17 00:03:15 +0800

    feat: 调音器有了

--- src/routes/gadget/index.svelte
+++ src/routes/gadget/index.svelte
@@ -2,24 +2,23 @@
 </script>
 
 <script>
-	// export let posts;
+  // export let posts;
 </script>
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
-</style>
-
 <svelte:head>
-	<title>Gadget</title>
+  <title>Gadget</title>
 </svelte:head>
 
 <h1>这里是我做的一些（自认为）有趣的小玩意儿</h1>
 
 <ul>
-	<li><a rel="prefetch" href="gadget/marked">marked</a></li>
-	<li><a rel="prefetch" href="gadget/tuner">Tuner</a></li>
-
+  <li><a rel="prefetch" href="gadget/marked">marked</a></li>
+  <li><a rel="prefetch" href="gadget/tuner">Tuner</a></li>
 </ul>
+
+<style>
+  ul {
+    margin: 0 0 1em 0;
+    line-height: 1.5;
+  }
+</style>
--- src/routes/gadget/tuner.svelte
+++ src/routes/gadget/tuner.svelte
@@ -1,33 +1,25 @@
 <script>
-import { connect } from "http2";
+  // 参考 https://www.jianshu.com/p/4fd8779943c3
+  AudioContext = window.AudioContext || window.webkitAudioContext;
 
-  export let audio;
+  const audioContext = new AudioContext();
+  const analyser = audioContext.createAnalyser();
+  const scriptProcessor = audioContext.createScriptProcessor(8192, 1, 1);
+
+  navigator.mediaDevices.getUserMedia({ audio: true }).then((streamSource) => {
+    // connect 的顺序一定要 streamSource => analyser => scriptProcessor
+    audioContext.createMediaStreamSource(streamSource).connect(analyser);
+    analyser.connect(scriptProcessor);
+    scriptProcessor.connect(audioContext.destination);
+
+    scriptProcessor.addEventListener("audioprocess", (event) => {
+      const data = event.inputBuffer.getChannelData(0);
+      // 为了避免卡住浏览器，只打印一些简单的数据
+      console.log(`${data.length}, ${data[0]}`);
+    });
+  });
 </script>
 
-<!-- <script>
-  // 创建Audio Context
-  var AudioContext = window.AudioContext || window.webkitAudioContext;
-  var audioCtx = new AudioContext();
-  // 创建Analyser Node
-  const analyser = audioCtx.createAnalyser();
-  AnalyserNode.fftSize = 512;
-  // 设置Source Node
-  const source = AudioContext.createMediaElementSource(audio);
-  // 将音频源关联到分析器；
-  SourceBuffer.connect(analyser);
-
-  // 获取频率数据
-  const bufferLength = AnalyserNode.frequencyBinCount;
-  const dataArray = new Unit8Array(bufferLength);
-
-  analyser.getByteFrequencyData(dataArray);
-
-  
-</script> -->
-
-
 <div style="height: 100%; width: 100%;">
-  <div>
-    <audio bind:this={audio} controls src=""/>     
-  </div>
+  <div>这是一个调音器</div>
 </div>
