b9cc7923b6d8401577254c4f89dd3f044c4a0795
zhangmingrui <zhangmingrui@mokahr.com>
2021-05-28 15:37:33 +0800

    feat: 试一下服务器端的https

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
-import sirv from "sirv";
-import polka from "polka";
-import compression from "compression";
-import * as sapper from "@sapper/server";
-
+import * as sapper from '@sapper/server';
+import compression from 'compression';
+import fs from 'fs';
+import { createServer } from 'https';
+import polka from 'polka';
+import sirv from 'sirv';
 const { NODE_ENV } = process.env;
-const dev = NODE_ENV === "development";
+const dev = NODE_ENV === 'development';
+const httpsOption = {
+  key: fs.readFileSync('./https/recluse.online.key'),
+  cert: fs.readFileSync('./https/recluse.online.pem'),
+};
+const { app } = polka() // You can also use Express
+  .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware());
 
-polka() // You can also use Express
-  .use(
-    compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware()
-  )
-  .listen("80", (err) => {
-    if (err) console.log("error", err);
-  });
+createServer(httpsOption, app).listen('80', (err) => {
+  if (err) console.log('error', err);
+});
+
+// polka() // You can also use Express
+//   .use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
+//   .listen('3000', (err) => {
+//     if (err) console.log('error', err);
+//   });
